92196d8a9098ffa48ed38e48ffac77157d52a556
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-07-24 16:21:59 +0100

    about

---
+++ pages/about/index.js
@@ -0,0 +1,12 @@
+import { useRouter } from "next/router"
+
+const AboutPage = () => {
+
+    return (
+        <div>
+            <h1>The AboutPage</h1>
+        </div>
+    )
+}
+
+export default AboutPage
